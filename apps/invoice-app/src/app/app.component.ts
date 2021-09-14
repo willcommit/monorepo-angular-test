95dc088c32cab9a3ba7147435947b3c767424376
William <welfversson@gmail.com>
2021-09-14 10:54:31 +0200

    initial commit

---
+++ apps/invoice-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'customer-portal-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'invoice-app';
+}
